1ba5fd786e6e8866cdbd4377dc39057348c08fe7
Valentina Baranova <v.sinekopova@gmail.com>
2022-10-17 16:31:12 +0300

    Initial commit

---
+++ Rectangle.cpp
@@ -0,0 +1,5 @@
+#include "Rectangle.h"
+
+fgr::Rectangle::Rectangle(int a, int b) : Parallelogram(a, b, 90) {
+    name = "Прямоугольник";
+};
---
+++ Rectangle.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Parallelogram.h"
+
+namespace fgr {
+
+    class Rectangle : public Parallelogram {
+    public:
+        Rectangle(int a, int b);
+    };
+}
